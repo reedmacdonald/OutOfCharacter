328104c8b76ded2baed0421811941aa52d7d00e6
Reed MacDonald <reedpmacdonald@gmail.com>
2020-05-24 23:44:20 -0700

    Add first room stuff

--- src/FirstRoom.js
+++ src/FirstRoom.js
@@ -12,74 +12,200 @@ function FirstRoom(props) {
   const [numPeople, setNumPeople] = React.useState(2);
   const [rotationSpeed, setRotationSpeed] = React.useState("slow");
   const [insanityLevel, setInsanityLevel] = React.useState(1);
+  const [showInstructions, setShowInstructions] = React.useState(false);
+  const [showAbout, setShowAbout] = React.useState(false);
   return (
-    <div className="App">
-      <h1 id="title">Out of Character</h1>
-      <h2 className="subTitle" style={{ color: "blue" }}>
-        Giving Video-Call Conversations a Kick
-      </h2>
-      <h3>How many people? (2-10)</h3>
-      <input
-        onChange={(e) => {
-          setNumPeople(e.target.value);
-        }}
-        type="number"
-        min="2"
-        max="10"
-      ></input>
+    <>
+      {showInstructions && (
+        <div
+          style={{
+            position: "absolute",
+            width: "35%",
+            height: "35%",
+            backgroundColor: "whiteSmoke",
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            border: "5px solid black",
+            borderRadius: "20px",
+            overflow: "scroll",
+            padding: "15px",
+          }}
+        >
+          <h3>
+            The goal of this web-app is to create lively conversation on video
+            chats by giving the participants "prompts" which contain things to
+            do during them. In Absurdity Levels 1 & 2, the "prompts" are pretty
+            mild so you could probably play this in most settings without anyone
+            noticing anything weird. Absurdity Level 3 and other game types get
+            pretty wild and are recommended to be played in settings where
+            everyone is playing.
+          </h3>
+          <h3>
+            Prompts will rotate randomly every minute to a minute and a half.
+            You can click "pass" to get a new prompt, though you only get two of
+            these per conversation. If you've completed your task, you can click
+            the check box so that it will be recorded and shown at the end. When
+            you have checked the box, this will not make the next prompt come
+            any faster. This is to keep the conversation from getting too zany,
+            too quickly.
+          </h3>
+          <h3>
+            The game is over when Player One chooses to end it. Player one is
+            the player who started the game and sent everyone the links. They
+            are the only one who sees the button to end it. Though it is not
+            required, it is recommended for games to be ended on a good note.
+          </h3>
+          <h3>
+            After the game is ended, everyone should see a new screen. Click
+            "See next player", to see what prompts each player got and whether
+            they accomplished them. Every player's "Player number" is in the URL
+            next to the topic, after the second-to-last dash. It is also the
+            order in which you sent the links. Player 1 can then either start a
+            new game (Screens should automatically refresh in this case, no need
+            to send new links) or end the game.
+          </h3>
+          <h3>Have fun and watch the demo if you have any questions</h3>
+          <span
+            className="clickThis"
+            onClick={() => {
+              setShowInstructions(false);
+            }}
+          >
+            (Click this to close)
+          </span>
+        </div>
+      )}
+      {showAbout && (
+        <div
+          style={{
+            position: "absolute",
+            width: "35%",
+            height: "35%",
+            backgroundColor: "whiteSmoke",
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            border: "5px solid black",
+            borderRadius: "20px",
+            overflow: "scroll",
+            padding: "15px",
+          }}
+        >
+          <h3>
+            This app was created with React and Firebase and deployed with
+            Netlify
+          </h3>
+          <h3>
+            The app was coded by Reed MacDonald (myself) and the prompts were
+            written by me and my cousin. I am not currently looking for a job
+            but you can email me about any business inquiries regarding the game
+            at reedpmacdonald@gmail.com.
+          </h3>
+          <span
+            className="clickThis"
+            onClick={() => {
+              setShowAbout(false);
+            }}
+          >
+            (Click this to close)
+          </span>
+        </div>
+      )}
+      <div className="App">
+        <h1 id="title">Out of Character</h1>
+        <h2 className="subTitle" style={{ color: "blue" }}>
+          Giving Video-Call Conversations a Kick
+        </h2>
+        <h3>How many people? (2-10)</h3>
+        <input
+          onChange={(e) => {
+            setNumPeople(e.target.value);
+          }}
+          type="number"
+          min="2"
+          max="10"
+        ></input>
 
-      <h3>Game Type</h3>
-      <select onChange={(e) => setInsanityLevel(e.target.value)}>
-        <option value={1}>Absurdity Level 1</option>
-        <option value={2}>Absurdity Level 2</option>
-        <option value={3}>Absurdity Level 3</option>
-        <option value={"characterBased"}>Character Based</option>
-        <option value={"politics"}>Politics</option>
-      </select>
-      <br />
-      <button
-        onClick={() => {
-          let randThing = Math.floor(Math.random() * 11);
-          createRoom(gameContext.roomNumber.toString(), {
-            numPeople,
-            rotationSpeed,
-            insanityLevel,
-            randThing,
-            player1Passes: 2,
-            player2Passes: 2,
-            player3Passes: 2,
-            player4Passes: 2,
-            player5Passes: 2,
-            player6Passes: 2,
-            player7Passes: 2,
-            player8Passes: 2,
-            player9Passes: 2,
-            player10Passes: 2,
-            task: ["Make some small-talk to get things going"],
-            taskPlayer2: ["Make some small-talk to get things going"],
-            taskPlayer3: ["Make some small-talk to get things going"],
-            taskPlayer4: ["Make some small-talk to get things going"],
-            taskPlayer5: ["Make some small-talk to get things going"],
-            taskPlayer6: ["Make some small-talk to get things going"],
-            taskPlayer7: ["Make some small-talk to get things going"],
-            taskPlayer8: ["Make some small-talk to get things going"],
-            taskPlayer9: ["Make some small-talk to get things going"],
-            taskPlayer10: ["Make some small-talk to get things going"],
-            turnNumber: 1,
-            gameOver: false,
-            unveil: 0,
-            topic: "Calibrating",
-            endGame: false,
-          });
-          //sessionStorage.setItem("yourRoom", gameContext.roomNumber);
-          props.history.push(
-            `/waitingroom/${gameContext.roomNumber}/${insanityLevel}`
-          );
-        }}
-      >
-        Start Game
-      </button>
-    </div>
+        <h3>Game Type:</h3>
+        <select
+          id="otherSelect"
+          onChange={(e) => setInsanityLevel(e.target.value)}
+        >
+          <option value={1}>Absurdity Level 1</option>
+          <option value={2}>Absurdity Level 2</option>
+          <option value={3}>Absurdity Level 3</option>
+          <option value={"characterBased"}>Character Based</option>
+          <option value={"politics"}>Politics</option>
+        </select>
+        <br />
+        <button
+          onClick={() => {
+            let randThing = Math.floor(Math.random() * 11);
+            createRoom(gameContext.roomNumber.toString(), {
+              numPeople,
+              rotationSpeed,
+              insanityLevel,
+              randThing,
+              player1Passes: 2,
+              player2Passes: 2,
+              player3Passes: 2,
+              player4Passes: 2,
+              player5Passes: 2,
+              player6Passes: 2,
+              player7Passes: 2,
+              player8Passes: 2,
+              player9Passes: 2,
+              player10Passes: 2,
+              task: ["Make some small-talk to get things going"],
+              taskPlayer2: ["Make some small-talk to get things going"],
+              taskPlayer3: ["Make some small-talk to get things going"],
+              taskPlayer4: ["Make some small-talk to get things going"],
+              taskPlayer5: ["Make some small-talk to get things going"],
+              taskPlayer6: ["Make some small-talk to get things going"],
+              taskPlayer7: ["Make some small-talk to get things going"],
+              taskPlayer8: ["Make some small-talk to get things going"],
+              taskPlayer9: ["Make some small-talk to get things going"],
+              taskPlayer10: ["Make some small-talk to get things going"],
+              turnNumber: 1,
+              gameOver: false,
+              unveil: 0,
+              topic: "Calibrating",
+              endGame: false,
+            });
+            //sessionStorage.setItem("yourRoom", gameContext.roomNumber);
+            props.history.push(
+              `/waitingroom/${gameContext.roomNumber}/${insanityLevel}`
+            );
+          }}
+        >
+          Start Game
+        </button>
+        <div id="homepageDiv">
+          <span
+            onClick={() => {
+              setShowInstructions(true);
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            Instructions
+          </span>
+          <br />
+          <span>Demo</span>
+          <br />
+          <span
+            onClick={() => {
+              setShowAbout(true);
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            About
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
 
