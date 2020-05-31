2d115d254143bcc1ba07a39f9a633a9388718806
Reed MacDonald <reedpmacdonald@gmail.com>
2020-05-31 11:04:08 -0700

    Add date and family to options at the end

--- src/EndRoom.js
+++ src/EndRoom.js
@@ -379,6 +379,8 @@ const EndRoom = (props) => {
                 <option value={3}>Absurdity Level 3</option>
                 <option value={"characterBased"}>Character Based</option>
                 <option value={"politics"}>Politics</option>
+                <option value={"date"}>First Date Icebreaker</option>
+                <option value={"family"}>Family Reunion</option>
               </select>
             </div>
             <br />
